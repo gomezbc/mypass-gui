9f83dad11b2bf56040cbc74f45433438e3625bf1
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-01-15 10:09:14 +0100

    Added trait for repository

--- src-tauri/src/db/mod.rs
+++ src-tauri/src/db/mod.rs
@@ -1,3 +1,14 @@
+use std::fmt::Result;
+
 pub mod connection;
 pub mod login_repo;
 pub mod master_key_repo;
+
+pub trait Repository<T> {
+    fn insert(&self, t: T) -> Result;
+    fn insert_many(&self, t: Vec<T>) -> Result;
+    fn find(&self, t: T) -> Result;
+    fn find_all(&self) -> Result;
+    fn update(&self, t: T) -> Result;
+    fn delete(&self, t: T) -> Result;
+}
