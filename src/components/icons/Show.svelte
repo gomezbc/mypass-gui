c867d6d8e496e27ea81ed12e8c8448a51389446a
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-01-16 19:30:23 +0100

    Refactor credential entry component and add login entry component

---
+++ src/components/icons/Show.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let className: string;
+</script>
+
+<svg
+  fill="currentColor"
+  viewBox="0 0 24 24"
+  xmlns="http://www.w3.org/2000/svg"
+  class={className}
+  ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+    id="SVGRepo_tracerCarrier"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></g><g id="SVGRepo_iconCarrier"
+    ><path
+      d="M12 5c-7.633 0-9.927 6.617-9.948 6.684L1.946 12l.105.316C2.073 12.383 4.367 19 12 19s9.927-6.617 9.948-6.684l.106-.316-.105-.316C21.927 11.617 19.633 5 12 5zm0 11c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4z"
+    ></path><path
+      d="M12 10c-1.084 0-2 .916-2 2s.916 2 2 2 2-.916 2-2-.916-2-2-2z"
+    ></path></g
+  ></svg
+>
