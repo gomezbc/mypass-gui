31bf401bcf2493ebafe4d792193fc38458f2ddf9
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-01-16 18:15:23 +0100

    added dropdown entry for credential list

---
+++ src/components/CredentialEntry.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import ArrowDown from "@/components/icons/arrowDown.svelte";
+
+    export let domain: string;
+    export let email: string;
+    export let username: string;
+    export let password: string;
+
+    let isOpen = false;
+
+    function toggleDropdown() {
+        isOpen = !isOpen;
+    }
+</script>
+
+<li>
+    <button class="flex items-center"  on:click={toggleDropdown}>
+        <ArrowDown className="size-3"/>
+        <span class="ml-2">{domain}</span>
+    </button>
+    {#if isOpen}
+        <div>
+            <span>{email}</span><br />
+            <span>{username}</span><br />
+            <span>{password}</span>
+        </div>
+    {/if}
+</li>
---
+++ src/components/icons/arrowDown.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let className: string;
+</script>
+<svg
+  aria-hidden="true"
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 14 8"
+  class={className}
+>
+  <path
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"></path>
+</svg>
