41651beee3940149d39a58331d6093609c462c85
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-01-16 18:26:07 +0100

    dropdown transition

--- src/components/CredentialEntry.svelte
+++ src/components/CredentialEntry.svelte
@@ -1,28 +1,29 @@
 <script lang="ts">
-    import ArrowDown from "@/components/icons/arrowDown.svelte";
+  import { slide } from "svelte/transition";
+  import ArrowDown from "@/components/icons/arrowDown.svelte";
 
-    export let domain: string;
-    export let email: string;
-    export let username: string;
-    export let password: string;
+  export let domain: string;
+  export let email: string;
+  export let username: string;
+  export let password: string;
 
-    let isOpen = false;
+  let isOpen = false;
 
-    function toggleDropdown() {
-        isOpen = !isOpen;
-    }
+  function toggleDropdown() {
+    isOpen = !isOpen;
+  }
 </script>
 
 <li>
-    <button class="flex items-center"  on:click={toggleDropdown}>
-        <ArrowDown className="size-3"/>
-        <span class="ml-2">{domain}</span>
-    </button>
-    {#if isOpen}
-        <div>
-            <span>{email}</span><br />
-            <span>{username}</span><br />
-            <span>{password}</span>
-        </div>
-    {/if}
+  <button class="flex items-center" on:click={toggleDropdown}>
+    <ArrowDown className="size-3" />
+    <span class="ml-2">{domain}</span>
+  </button>
+  {#if isOpen}
+    <div transition:slide={{ duration: 200 }}>
+      <span>{email}</span><br />
+      <span>{username}</span><br />
+      <span>{password}</span>
+    </div>
+  {/if}
 </li>
