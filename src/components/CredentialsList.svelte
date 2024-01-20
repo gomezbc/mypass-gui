74450e7aadd7b2e9695eb9574934806364f85b51
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-01-20 18:47:47 +0100

    Add Spinner component and update CredentialsList layout

--- src/components/CredentialsList.svelte
+++ src/components/CredentialsList.svelte
@@ -2,6 +2,7 @@
   import { invoke } from "@tauri-apps/api/tauri";
 
   import LoginEntry from "@/components/LoginEntry.svelte";
+  import Spinner from "@/components/Spinner.svelte";
 
   type Login = {
     domain: string;
@@ -20,19 +21,26 @@
   }
 
   let logins: Login[] = [];
+  let isLoading = true;
 
   getLogins().then((result) => {
+    isLoading = true;
     logins = result;
     logins.forEach((login) => {
       login.credentials.forEach((credential) => {
         credential.pass = "•".repeat(8);
       });
     });
+    isLoading = false;
   });
 </script>
 
-<ul>
-  {#each logins as login}
-    <LoginEntry domain={login.domain} credentials={login.credentials} />
-  {/each}
-</ul>
+{#if isLoading}
+  <Spinner />
+{:else}
+  <ul class="w-full">
+    {#each logins as login}
+      <LoginEntry domain={login.domain} credentials={login.credentials} />
+    {/each}
+  </ul>
+{/if}
