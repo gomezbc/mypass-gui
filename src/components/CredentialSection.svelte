fc2081a46f31b36781b221e81bc6cfd06dde28dc
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-01-21 13:44:26 +0100

    Added a table for credentials, before adapting it to my requirements

---
+++ src/components/CredentialSection.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import CredentialFooter from "./CredentialFooter.svelte";
+import CredentialHeader from "./CredentialHeader.svelte";
+  import CredentialTable from "./CredentialTable.svelte";
+</script>
+
+<!-- Table Section -->
+<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+  <!-- Card -->
+  <div class="flex flex-col">
+    <div class="-m-1.5 overflow-x-auto">
+      <div class="p-1.5 min-w-full inline-block align-middle">
+        <div
+          class="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden dark:bg-slate-900 dark:border-gray-700"
+        >
+          <CredentialHeader />
+          <CredentialTable />
+          <CredentialFooter />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
