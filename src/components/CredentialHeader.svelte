7e93ad1061c59c026fd7f4fb620fb5400b1f2178
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-01-21 18:41:18 +0100

    Added flowbite to svelte and created ui modal to add new credentials

--- src/components/CredentialHeader.svelte
+++ src/components/CredentialHeader.svelte
@@ -1,3 +1,7 @@
+<script>
+  import NewCredentialModal from "./NewCredentialModal.svelte";
+</script>
+
 <div
   class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-b border-gray-200 dark:border-gray-700"
 >
@@ -18,28 +22,7 @@
       >
         View all
       </a>
-
-      <a
-        class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-        href="#"
-      >
-        <svg
-          class="flex-shrink-0 w-3 h-3"
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-          fill="none"
-        >
-          <path
-            d="M2.63452 7.50001L13.6345 7.5M8.13452 13V2"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-          />
-        </svg>
-        Add credential
-      </a>
+      <NewCredentialModal />
     </div>
   </div>
 </div>
---
+++ src/components/NewCredentialModal.svelte
@@ -0,0 +1,62 @@
+<script>
+  import { Button, Modal, Label, Input } from "flowbite-svelte";
+  let formModal = false;
+</script>
+
+<Button color="blue" on:click={() => (formModal = true)}>
+  <svg
+    class="flex-shrink-0 w-3 h-3"
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+  >
+    <path
+      d="M2.63452 7.50001L13.6345 7.5M8.13452 13V2"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+  </svg> <span class="ml-2">Add Credential</span>
+</Button>
+
+<Modal bind:open={formModal} size="xs" autoclose={false} class="w-full">
+  <form class="flex flex-col space-y-6" action="#">
+    <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+      Add new credential
+    </h3>
+    <Label class="space-y-2">
+      <span>Domain</span>
+      <Input
+        type="text"
+        name="domain"
+        placeholder="example.com"
+        required
+      />
+    </Label>
+    <Label class="space-y-2">
+      <span>Email</span>
+      <Input
+        type="email"
+        name="email"
+        placeholder="name@company.com"
+        required
+      />
+    </Label>
+    <Label class="space-y-2">
+      <span>Username</span>
+      <Input
+        type="text"
+        name="username"
+        placeholder="username"
+        required
+      />
+    </Label>
+    <Label class="space-y-2">
+      <span>Your password</span>
+      <Input type="password" name="password" placeholder="••••••••" required />
+    </Label>
+    <Button color="blue" type="submit" class="w-full1">Add Credential</Button>
+  </form>
+</Modal>
