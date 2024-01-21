4221a1b4b86241afe2b108eb03d0930afb1b9ed5
gomezbc <77118356+gomezbc@users.noreply.github.com>
2024-01-21 13:50:17 +0100

    Created separated component for credentials entry

---
+++ src/components/CredentialTableEntry.svelte
@@ -0,0 +1,77 @@
+<tr>
+  <td class="h-px w-px whitespace-nowrap">
+    <div class="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3">
+      <div class="flex items-center gap-x-3">
+        <div class="grow">
+          <span
+            class="block text-sm font-semibold text-gray-800 dark:text-gray-200"
+            >Christina Bersh</span
+          >
+        </div>
+      </div>
+    </div>
+  </td>
+  <td class="h-px w-72 whitespace-nowrap">
+    <div class="px-6 py-3">
+      <span class="block text-sm font-semibold text-gray-800 dark:text-gray-200"
+        >Director</span
+      >
+      <span class="block text-sm text-gray-500">Human resources</span>
+    </div>
+  </td>
+  <td class="h-px w-px whitespace-nowrap">
+    <div class="px-6 py-3">
+      <span
+        class="py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium bg-teal-100 text-teal-800 rounded-full dark:bg-teal-500/10 dark:text-teal-500"
+      >
+        <svg
+          class="w-2.5 h-2.5"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"
+          />
+        </svg>
+        Active
+      </span>
+    </div>
+  </td>
+  <td class="h-px w-px whitespace-nowrap">
+    <div class="px-6 py-3">
+      <div class="flex items-center gap-x-3">
+        <span class="text-xs text-gray-500">1/5</span>
+        <div
+          class="flex w-full h-1.5 bg-gray-200 rounded-full overflow-hidden dark:bg-gray-700"
+        >
+          <div
+            class="flex flex-col justify-center overflow-hidden bg-gray-800 dark:bg-gray-200"
+            role="progressbar"
+            style="width: 25%"
+            aria-valuenow="25"
+            aria-valuemin="0"
+            aria-valuemax="100"
+          ></div>
+        </div>
+      </div>
+    </div>
+  </td>
+  <td class="h-px w-px whitespace-nowrap">
+    <div class="px-6 py-3">
+      <span class="text-sm text-gray-500">28 Dec, 12:12</span>
+    </div>
+  </td>
+  <td class="h-px w-px whitespace-nowrap">
+    <div class="px-6 py-1.5">
+      <a
+        class="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+        href="#"
+      >
+        Edit
+      </a>
+    </div>
+  </td>
+</tr>
